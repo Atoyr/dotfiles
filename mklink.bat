ec38629b34ce9c7956e6716df115b962cc27cdc0
Atoyr <lessironglance@gmail.com>
2018-09-01 18:03:56 +0900

    mod filepath and comment out

--- mklink.bat
+++ mklink.bat
@@ -1,5 +1,5 @@
-mklink %HOMEPATH%"\.vimrc" %HOMEPATH%"\dotfiles\.vimrc"
-mklink /D %HOMEPATH%"\.vim\ftdetect" %HOMEPATH%"\dotfiles\ftdetect"
-mklink /D %HOMEPATH%"\.vim\indent" %HOMEPATH%"\dotfiles\indent"
-mklink /D %HOMEPATH%"\.vim\colors" %HOMEPATH%"\dotfiles\colors"
+mklink %HOMEPATH%"\.vimrc" %HOMEPATH%"\dotfiles\vimrc"
+rem mklink /D %HOMEPATH%"\.vim\ftdetect" %HOMEPATH%"\dotfiles\ftdetect"
+rem mklink /D %HOMEPATH%"\.vim\indent" %HOMEPATH%"\dotfiles\indent"
+rem mklink /D %HOMEPATH%"\.vim\colors" %HOMEPATH%"\dotfiles\colors"
 exit 0
