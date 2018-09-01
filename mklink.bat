8a4586ce4370c27efc19100d44d5ce725927545a
Atoyr <lessironglance@gmail.com>
2018-09-01 18:07:57 +0900

    mod mklink script

--- mklink.bat
+++ mklink.bat
@@ -1,4 +1,4 @@
-mklink %HOMEPATH%"\.vimrc" %HOMEPATH%"\dotfiles\vimrc"
+mklink "%HOMEPATH%\vimfiles\vimrc" "%HOMEPATH%\dotfiles\.vimrc"
 rem mklink /D %HOMEPATH%"\.vim\ftdetect" %HOMEPATH%"\dotfiles\ftdetect"
 rem mklink /D %HOMEPATH%"\.vim\indent" %HOMEPATH%"\dotfiles\indent"
 rem mklink /D %HOMEPATH%"\.vim\colors" %HOMEPATH%"\dotfiles\colors"
