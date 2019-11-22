02b3bfe82366df3704e962bb4ce9975c5eebc4b5
Uchiyama Ryota <r.uchiyama@earth.sint.co.jp>
2019-11-22 11:13:59 +0900

    🐛path

--- mklink.bat
+++ mklink.bat
@@ -1,4 +1,5 @@
 mklink "%HOME%\vimfiles\vimrc" "%HOME%\dotfiles\.vimrc"
+mklink "%HOME%\vimfiles\gvimrc" "%HOME%\dotfiles\gvimrc"
 mklink "%HOME%\.bashrc" "%HOME%\dotfiles\.bashrc"
 mklink "%HOME%\.tmux.conf" "%HOME%\dotfiles\.tmux.conf" 
 mklink /d "%HOME%\.config" "%HOME%\dotfiles\.config" 
