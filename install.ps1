7769156280c84d03794f2c809b93ba6187739c67
UCHIYAMA Ryota <lessironglance@gmail.com>
2023-02-12 00:37:53 +0900

    fix env

--- install.ps1
+++ install.ps1
@@ -23,8 +23,8 @@ function Write-Help {
 
 function Set-UserEnvironmentVariable {
     Write-Info "Set User environment variable."
-    [Environment]::SetEnvironmentVariable('XDG_CONFIG_HOME', '%USERPROFILE%\.config', 'User')
-    [Environment]::SetEnvironmentVariable('XDG_DATA_HOME', '%USERPROFILE%\.local\share', 'User')
+    [Environment]::SetEnvironmentVariable('XDG_CONFIG_HOME', "$env:USERPROFILE\.config", 'User')
+    [Environment]::SetEnvironmentVariable('XDG_DATA_HOME', "$env:USERPROFILE\.local\share", 'User')
 }
 
 # creating symbolic link
