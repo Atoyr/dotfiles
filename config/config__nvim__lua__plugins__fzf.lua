ba5a85dc1cfaa46b3868ddc566cd088e093930da
UCHIYAMA Ryota <lessironglance@gmail.com>
2024-03-23 19:43:27 +0900

    fix fzf plugin rewrited lazy.nvim

--- config/config__nvim__lua__plugins__fzf.lua
+++ config/config__nvim__lua__plugins__fzf.lua
@@ -2,43 +2,39 @@
 return {
   {
     "nvim-telescope/telescope.nvim",
-      module = { "telescope" },
-      requires = { "nvim-lua/plenary.nvim" },
-      wants = { },
-      setup = function()
-        local function builtin(name)
+    dependencies = { "nvim-lua/plenary.nvim" },
+    init = function()
+      local function builtin(name)
 
-          return function(opt)
-            return function()
+        return function(opt)
+          return function()
             return require("telescope.builtin")[name](opt or {})
             end
-          end
-
         end
 
-        local function extensions(name, prop)
-          return function(opt)
+      end
 
-            return function()
+      local function extensions(name, prop)
+        return function(opt)
 
-              local telescope = require "telescope"
-              telescope.load_extension(name)
-              return telescope.extensions[name][prop](opt or {})
-            end
+          return function()
+
+            local telescope = require "telescope"
+            telescope.load_extension(name)
+            return telescope.extensions[name][prop](opt or {})
           end
         end
+      end
 
 
-        vim.keymap.set("n", "<Leader>ff", builtin "find_files" {})
-        vim.keymap.set("n", "<Leader>fg", builtin "live_grep" {})
-        vim.keymap.set("n", "<Leader>fh", builtin "help_tags" { lang = "en" })
+      vim.keymap.set("n", "<Leader>ff", builtin "find_files" {})
+      vim.keymap.set("n", "<Leader>fg", builtin "live_grep" {})
+      vim.keymap.set("n", "<Leader>fh", builtin "help_tags" { lang = "ja" })
 
-        vim.keymap.set("n", "<Leader>fb", builtin "buffers" {})
-        end,
-      config = function()
-        require("telescope").setup {
-
-        }
+      vim.keymap.set("n", "<Leader>fb", builtin "buffers" {})
+    end,
+    config = function()
+      require("telescope").setup { }
     end
   }
 }
