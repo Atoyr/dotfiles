9f786dd4906dec3f037ca2b7cc7b6fc0e36b02d2
UCHIYAMA Ryota <lessironglance@gmail.com>
2024-09-27 08:24:29 +0900

    update add copilot chat

--- config/config__nvim__lua__plugins__copilot.lua
+++ config/config__nvim__lua__plugins__copilot.lua
@@ -1,20 +1,36 @@
 return {
- "zbirenbaum/copilot.lua", 
- opts = {
-   panel = {
-    enabled = true,
-    auto_refresh = false,
-    keymap = {
-      jump_prev = "<C-k>",
-      jump_next = "<C-j>",
-      accept = "<C-g>",
-      refresh = "gr",
-      open = "<C-s>"
-    },
-    layout = {
-      position = "bottom", -- | top | left | right
-      ratio = 0.4
-    },
+  {
+   "zbirenbaum/copilot.lua", 
+   opts = {
+     panel = {
+      enabled = true,
+      auto_refresh = false,
+      keymap = {
+        jump_prev = "<C-k>",
+        jump_next = "<C-j>",
+        accept = "<C-g>",
+        refresh = "gr",
+        open = "<C-s>"
+      },
+      layout = {
+        position = "bottom", -- | top | left | right
+        ratio = 0.4
+      },
+     }
    }
- }
+  }, 
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+    },
+    build = "make tiktoken", -- Only on MacOS or Linux
+    opts = {
+      debug = true, -- Enable debugging
+      -- See Configuration section for rest
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
 }
