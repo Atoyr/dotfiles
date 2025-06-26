77fdec4055a24f35e05d911c79baef5c0ca42c2c
UCHIYAMA Ryota <lessironglance@gmail.com>
2025-06-27 08:43:53 +0900

    update: golang settings

--- config/zsh.d__golang.zsh
+++ config/zsh.d__golang.zsh
@@ -4,3 +4,9 @@ if [ -d /usr/local/go/bin ]; then
   export PATH=$PATH:~/go/bin
 fi
 
+if [ -d $HOME/.goenv ]; then
+  export GOENV_ROOT="$HOME/.goenv"
+  export PATH="$GOENV_ROOT/bin:$PATH"
+  eval "$(goenv init -)"
+fi
+
