00fe518de9823c3510dc521c437826207c92a707
UCHIYAMA Ryota <lessironglance@gmail.com>
2025-10-05 00:11:15 +0900

    update: add formatter fy

--- config/config__nvim__lua__plugins__formatter.lua
+++ config/config__nvim__lua__plugins__formatter.lua
@@ -22,13 +22,22 @@ return {
       lua = { "stylua" },
       python = { "isort", "black" },
       javascript = { "prettierd", "prettier", stop_after_first = true },
+      javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+      typescript = { "prettierd", "prettier", stop_after_first = true },
+      vue = { "prettierd", "prettier", stop_after_first = true },
+      json = { "prettierd", "prettier", stop_after_first = true },
+      css = { "prettierd", "prettier", stop_after_first = true },
     },
     -- Set default options
     default_format_opts = {
       lsp_format = "fallback",
     },
     -- Set up format-on-save
-    format_on_save = { timeout_ms = 500 },
+    format_on_save = {
+      timeout_ms = 500,
+      lsp_fallback = true,
+      quiet = false
+    },
     -- Customize formatters
     formatters = {
       shfmt = {
