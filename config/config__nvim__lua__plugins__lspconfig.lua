4e4442d58388a8c2e9b441fd79b2f7dc8d5cf0b6
UCHIYAMA Ryota <lessironglance@gmail.com>
2025-10-04 12:28:31 +0900

    fix: rm cs config

--- config/config__nvim__lua__plugins__lspconfig.lua
+++ config/config__nvim__lua__plugins__lspconfig.lua
@@ -1,11 +1,5 @@
 return {
   { 'neovim/nvim-lspconfig' }, 
-  { 'OmniSharp/omnisharp-vim', 
-    init = function()
-      vim.g.OmniSharp_server_use_net6 = 1
-      vim.g.OmniSharp_server_use_mono = 0
-    end,
-  }, 
   {
     'mason-org/mason.nvim',
     version = "^1.0.0", 
