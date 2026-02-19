d557f7250f94be31fc37c6f479de38d45fe61b2d
UCHIYAMA Ryota <lessironglance@gmail.com>
2026-02-20 01:25:09 +0900

    update: copilot

--- config/.config/nvim/lua/plugins/cmp.lua
+++ config/.config/nvim/lua/plugins/cmp.lua
@@ -1,25 +1,17 @@
-return{ 
-  {
-    "giuxtaposition/blink-cmp-copilot",
-  }, 
+-- ~/.config/nvim/lua/plugins/copilot.lua
+return {
+  -----------------------------------------------------------
+  -- blink.cmp + Copilotソース
+  -----------------------------------------------------------
   {
     "saghen/blink.cmp",
-    dependencies = {
-      {
-        "giuxtaposition/blink-cmp-copilot",
-      },
-    },
     version = "1.*",
-    ---@module 'blink.cmp'
-    ---@type blink.cmp.Config
+    dependencies = {
+      "giuxtaposition/blink-cmp-copilot",
+    },
     opts = {
-      keymap = { preset = "enter" },
-      appearance = {
-        nerd_font_variant = "mono",
-      },
-      completion = { documentation = { auto_show = true } },
       sources = {
-        default = { "lsp", "path", "snippets", "buffer", "copilot"},
+        default = { "lsp", "path", "snippets", "buffer", "copilot" },
         providers = {
           copilot = {
             name = "copilot",
@@ -39,46 +31,120 @@ return{
         },
       },
       appearance = {
-        -- Blink does not expose its default kind icons so you must copy them all (or set your custom ones) and add Copilot
         kind_icons = {
-          Copilot = "",
-          Text = '󰉿',
-          Method = '󰊕',
-          Function = '󰊕',
-          Constructor = '󰒓',
-
-          Field = '󰜢',
-          Variable = '󰆦',
-          Property = '󰖷',
-
-          Class = '󱡠',
-          Interface = '󱡠',
-          Struct = '󱡠',
-          Module = '󰅩',
-
-          Unit = '󰪚',
-          Value = '󰦨',
-          Enum = '󰦨',
-          EnumMember = '󰦨',
-
-          Keyword = '󰻾',
-          Constant = '󰏿',
-
-          Snippet = '󱄽',
-          Color = '󰏘',
-          File = '󰈔',
-          Reference = '󰬲',
-          Folder = '󰉋',
-          Event = '󱐋',
-          Operator = '󰪚',
-          TypeParameter = '󰬛',
+          Copilot = "",
+          Text = "󰉿",
+          Method = "󰊕",
+          Function = "󰊕",
+          Constructor = "󰒓",
+          Field = "󰜢",
+          Variable = "󰆦",
+          Property = "󰖷",
+          Class = "󱡠",
+          Interface = "󱡠",
+          Struct = "󱡠",
+          Module = "󰅩",
+          Unit = "󰪚",
+          Value = "󰦨",
+          Enum = "󰦨",
+          EnumMember = "󰦨",
+          Keyword = "󰻾",
+          Constant = "󰏿",
+          Snippet = "󱄽",
+          Color = "󰏘",
+          File = "󰈔",
+          Reference = "󰬲",
+          Folder = "󰉋",
+          Event = "󱐋",
+          Operator = "󰪚",
+          TypeParameter = "󰬛",
         },
-    },
-      fuzzy = {
-        -- versionを指定してないとバイナリが特定できずLuaにfallbackするwarningが表示される
-        implementation = "prefer_rust_with_warning",
       },
     },
-    opts_extend = { "sources.default" },
-  }
+  },
+
 }
+-- return{
+--   {
+--     "giuxtaposition/blink-cmp-copilot",
+--   },
+--   {
+--     "saghen/blink.cmp",
+--     dependencies = {
+--       {
+--         "giuxtaposition/blink-cmp-copilot",
+--       },
+--     },
+--     version = "1.*",
+--     ---@module 'blink.cmp'
+--     ---@type blink.cmp.Config
+--     opts = {
+--       keymap = { preset = "enter" },
+--       appearance = {
+--         nerd_font_variant = "mono",
+--       },
+--       completion = { documentation = { auto_show = true } },
+--       sources = {
+--         default = { "lsp", "path", "snippets", "buffer", "copilot"},
+--         providers = {
+--           copilot = {
+--             name = "copilot",
+--             module = "blink-cmp-copilot",
+--             score_offset = 100,
+--             async = true,
+--             transform_items = function(_, items)
+--               local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
+--               local kind_idx = #CompletionItemKind + 1
+--               CompletionItemKind[kind_idx] = "Copilot"
+--               for _, item in ipairs(items) do
+--                 item.kind = kind_idx
+--               end
+--               return items
+--             end,
+--           },
+--         },
+--       },
+--       appearance = {
+--         -- Blink does not expose its default kind icons so you must copy them all (or set your custom ones) and add Copilot
+--         kind_icons = {
+--           Copilot = "",
+--           Text = '󰉿',
+--           Method = '󰊕',
+--           Function = '󰊕',
+--           Constructor = '󰒓',
+--
+--           Field = '󰜢',
+--           Variable = '󰆦',
+--           Property = '󰖷',
+--
+--           Class = '󱡠',
+--           Interface = '󱡠',
+--           Struct = '󱡠',
+--           Module = '󰅩',
+--
+--           Unit = '󰪚',
+--           Value = '󰦨',
+--           Enum = '󰦨',
+--           EnumMember = '󰦨',
+--
+--           Keyword = '󰻾',
+--           Constant = '󰏿',
+--
+--           Snippet = '󱄽',
+--           Color = '󰏘',
+--           File = '󰈔',
+--           Reference = '󰬲',
+--           Folder = '󰉋',
+--           Event = '󱐋',
+--           Operator = '󰪚',
+--           TypeParameter = '󰬛',
+--         },
+--     },
+--       fuzzy = {
+--         -- versionを指定してないとバイナリが特定できずLuaにfallbackするwarningが表示される
+--         implementation = "prefer_rust_with_warning",
+--       },
+--     },
+--     opts_extend = { "sources.default" },
+--   }
+-- }
--- config/.config/nvim/lua/plugins/copilot.lua
+++ config/.config/nvim/lua/plugins/copilot.lua
@@ -1,36 +1,66 @@
 return {
+  -----------------------------------------------------------
+  -- Copilot LSP (補完エンジン)
+  -----------------------------------------------------------
   {
-    "zbirenbaum/copilot.lua", 
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
     opts = {
-      panel = {
-        enabled = true,
-        auto_refresh = false,
-        keymap = {
-          jump_prev = "<C-k>",
-          jump_next = "<C-j>",
-          accept = "<C-g>",
-          refresh = "gr",
-          open = "<C-s>"
-        },
-        layout = {
-          position = "bottom", -- | top | left | right
-          ratio = 0.4
-        },
-      }
-    }
-  }, 
+      suggestion = { enabled = false }, -- blink.cmpに任せるので無効化
+      panel = { enabled = false },
+    },
+  },
   {
     "CopilotC-Nvim/CopilotChat.nvim",
     branch = "main",
     dependencies = {
       { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
-      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+      { "nvim-lua/plenary.nvim" },  -- for curl, log wrapper
     },
-    build = "make tiktoken", -- Only on MacOS or Linux
+    build = "make tiktoken",        -- Only on MacOS or Linux
     opts = {
-      debug = true, -- Enable debugging
+      debug = true,                 -- Enable debugging
       -- See Configuration section for rest
     },
     -- See Commands section for default commands if you want to lazy load on them
+  },
+
+  -----------------------------------------------------------
+  -- CopilotChat (Chat + Agent)
+  -----------------------------------------------------------
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "nvim-lua/plenary.nvim", branch = "master" },
+    },
+    build = "make tiktoken",
+    opts = {
+      model = "claude-3.5-sonnet",
+      window = {
+        layout = "vertical",
+        width = 0.4,
+      },
+      auto_insert_mode = true,
+    },
+    keys = {
+      { "<leader>cc", "<cmd>CopilotChatToggle<CR>",  desc = "CopilotChat Toggle" },
+      { "<leader>ce", "<cmd>CopilotChatExplain<CR>", mode = "v",                 desc = "Explain" },
+      { "<leader>cr", "<cmd>CopilotChatReview<CR>",  mode = "v",                 desc = "Review" },
+      { "<leader>cf", "<cmd>CopilotChatFix<CR>",     mode = "v",                 desc = "Fix" },
+      { "<leader>ct", "<cmd>CopilotChatTests<CR>",   mode = "v",                 desc = "Tests" },
+      {
+        "<leader>cq",
+        function()
+          local input = vim.fn.input("Quick Chat: ")
+          if input ~= "" then
+            require("CopilotChat").ask(input, {
+              selection = require("CopilotChat.select").buffer,
+            })
+          end
+        end,
+        desc = "Quick Chat",
+      },
+    },
   },
 }
